2f75302e50f0be8ce8c875d18fd2d0f0a4283437
Joshua Pauline <joshpauline@gmail.com>
2021-11-23 21:12:49 +1100

    update default card

--- src/handlers/users/setDefaultPaymentMethod.ts
+++ src/handlers/users/setDefaultPaymentMethod.ts
@@ -9,56 +9,35 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2020-08-27',
 });
 
-const getPaymentDetails = async (
+const setDefaultPaymentMethod = async (
   event: AWSLambda.APIGatewayEvent
 ): Promise<Response> => {
   try {
     const headers: APIGatewayProxyEventHeaders = event.headers;
     const decode: any = jwt_decode(headers.Authorization!);
     const userId = decode['custom:userId'];
+    const { paymentMethod } = event.pathParameters!;
 
     const [user]: any = await getUserById(userId);
 
     const { stripeId } = user;
 
-    const paymentMethods = (
-      await stripe.paymentMethods.list({
-        customer: stripeId,
-        type: 'card',
-        expand: ['data.customer'],
-      })
-    ).data.map((method) => {
-      const { id, customer }: any = method;
-
-      let defaultMethod = false;
-
-      if (
-        customer &&
-        customer.invoice_settings &&
-        customer.invoice_settings.default_payment_method === id
-      ) {
-        defaultMethod = true;
-      }
-      const { brand, exp_month, exp_year, last4 } = method.card!;
-      const card = {
-        brand,
-        expMonth: exp_month,
-        expYear: exp_year,
-        last4,
-        id,
-        defaultMethod,
-      };
-      return card;
+    const customerUpdate = await stripe.customers.update(stripeId, {
+      invoice_settings: {
+        default_payment_method: paymentMethod,
+      },
     });
 
+    console.log(customerUpdate);
+
     return corsSuccessResponse({
-      paymentMethods,
+      message: 'Successfully set default payment method',
     });
   } catch (err) {
     console.log(err);
   }
   const response = corsErrorResponse({
-    message: 'Error fetching payment methods',
+    message: 'Error adding payment method',
   });
 
   return response;
@@ -66,4 +45,4 @@ const getPaymentDetails = async (
 
 // runWarm function handles pings from the scheduler so you don't
 // have to put that boilerplate in your function.
-export default runWarm(getPaymentDetails);
+export default runWarm(setDefaultPaymentMethod);
